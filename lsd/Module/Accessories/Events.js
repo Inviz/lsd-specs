cb6e4ea6c954f4b9e536c1ea9ce91edfd7c2f028
Oleg Smirnoff <ctapbiumabp@gmail.com>
2011-05-25 15:32:18 +0300

    tests for events

---
+++ lsd/Module/Accessories/Events.js
@@ -0,0 +1,158 @@
+describe("LSD.Module.Accessories.Events", function() {
+  describe("#simple", function() {
+
+    it ("self", function() {
+      var clicked = false;
+      LSD.Widget.Test = new Class({
+        options: {
+          events: {
+            self: {
+              'touch': 'onTouch'
+            }
+          }
+        },
+
+        onTouch: function(event) {
+          clicked = true;
+        }
+      });
+
+      var element = new Element('test');
+      var widget = new LSD.Widget(element, {context:'widget'});
+
+      widget.fireEvent('touch');
+      expect(clicked).toBeTruthy();
+    });
+
+    it ("element", function() {
+      var clicked = false;
+      LSD.Widget.Test = new Class({
+        options: {
+          events: {
+            element: {
+              'click': 'onClick'
+            }
+          }
+        },
+
+        onClick: function(event) {
+          clicked = true;
+        }
+      });
+
+      var element = new Element('test');
+      new LSD.Widget(element, {context:'widget'});
+
+      element.fireEvent("click");
+      expect(clicked).toBeTruthy();
+    });
+
+    it ("parent", function() {
+      var clicked = false;
+      LSD.Widget.Test = new Class({
+        options: {
+          events: {
+            parent: {
+              'click': 'onClick'
+            }
+          }
+        },
+
+        onClick: function(event) {
+          clicked = true;
+        }
+      });
+
+      var parent = new LSD.Widget(new Element('button'), {context:'widget'});
+      var widget = new LSD.Widget(new Element('test'), {context:'widget'});
+
+      parent.appendChild(widget);
+      parent.click();
+
+      expect(clicked).toBeTruthy();
+    });
+
+    it ("document", function() {
+      var clicked = false;
+
+      LSD.Widget.Test = new Class({
+        options: {
+          events: {
+            document: {
+              'click': 'onClick'
+            }
+          }
+        },
+
+        onClick: function(event) {
+          clicked = true;
+        }
+      });
+
+      var doc = new LSD.Widget({tag: 'doc'});
+      doc.document = doc;
+
+      var widget = new LSD.Widget(new Element('test'), {context:'widget'});
+      widget.inject(doc);
+
+      doc.fireEvent("click");
+      expect(clicked).toBeTruthy();
+    });
+
+    it ("window", function() {
+      var resized = false;
+
+      LSD.Widget.Test = new Class({
+        options: {
+          events: {
+            window: {
+              resize: 'onWindowResize'
+            }
+          }
+        },
+
+        onWindowResize: function(event) {
+          resized = true;
+        }
+      });
+
+      new LSD.Widget(new Element('test'), {context:'widget'});
+
+      window.fireEvent("resize");
+      expect(resized).toBeTruthy();
+    });
+
+  });
+
+  describe("#complex", function() {
+
+    it ("click:relay", function() {
+      var clicked = false;
+      LSD.Widget.Test = new Class({
+        options: {
+          events: {
+            element: {
+              'click:relay(button)': 'onClick'
+            }
+          }
+        },
+
+        onClick: function(event) {
+          clicked = true;
+        }
+      });
+
+
+      var widget = new LSD.Widget(new Element('test'), {context:'widget'});
+      var button = new LSD.Widget({tag: 'button'});
+
+      button.inject(widget);
+      widget.element.fireEvent('click', {target: button.element});
+
+      expect(clicked).toBeTruthy();
+    });
+
+  });
+
+
+});
